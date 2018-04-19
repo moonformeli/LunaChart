2e48445e3c5f1db4ffc9deafd71ea5dae3f19419
moonformeli <mgyang95@gmail.com>
2018-04-19 13:59:24 +0900

    Improvement - LunaChart
    Make basic instantiating methods in LunaChart

--- chart/LunaChart.js
+++ chart/LunaChart.js
@@ -1,25 +1,61 @@
 function CanvasChart() {
-    var Chart = function() {};
-
-    Chart.prototype.init = function() {
-        console.debug('init');
-        this.initProperty();
+    var Chart = function(args) {
+        this.init(args);
     };
 
-    Chart.prototype.initProperty = function() {
-        console.debug('initProperty');
-        this.minWidth  = 600;
-        this.minHeight = 300;
+    /**
+     *  Initiate arguments needed to draw a canvas on the target element, where the user wants to draw onto.
+     *  The parameter, args, basically needs these information below.
+     *
+     *  width
+     *  height
+     *  target - the element that the user wants to draw a chart onto
+     */
+    function initParam(args) {
+        var param = {};
+        var minWidth  = 600;
+        var minHeight = 300;
 
+        param.width  = args.width  || minWidth;
+        param.height = args.height || minHeight;
+        param.target = args.target || window;
+
+        return param;
+    }
+
+    Chart.prototype.init = function(args) {
+        console.debug('init');
+
+        this.initProperty(args);
+        this.initComponent();
+    };
+
+    Chart.prototype.initProperty = function(args) {
+        console.debug('initProperty');
+
+        this.param = initParam(args);
+    };
+
+    Chart.prototype.initComponent = function() {
         this.background = document.createElement("div");
         this.canvasAxis = document.createElement("canvas");
         this.ctxAxis    = this.canvasAxis.getContext("2d");
 
+        this.setWidth(this.param.width);
+        this.setHeight(this.param.height);
+
         this.background.appendChild(this.canvasAxis);
+        this.param.target.appendChild(this.background);
     };
 
     Chart.prototype.setWidth = function(w) {
+        this.background.style.width = w;
+        this.canvasAxis.style.width = w;
+    };
 
+    Chart.prototype.setHeight = function(h) {
+        this.background.style.height = h;
+        this.canvasAxis.style.height = h;
     };
 
     return Chart;
---
+++ js/index.js
@@ -0,0 +1,8 @@
+var main = document.createElement("div");
+document.body.appendChild(main);
+
+var chart = new LunaChart({
+    width  : 300,
+    height : 150,
+    target : main
+});
